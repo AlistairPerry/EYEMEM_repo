753df3e841de4c314059cf3c5b6cf4d9b3da0ccd
Perry <alistairperry123@gmail.com>
2018-07-26 17:44:36 +0200

    temp changes, fix later

--- B_preproc_prepareFM.sh
+++ B_preproc_prepareFM.sh
@@ -17,7 +17,7 @@ CurrentLog="${LogPath}/${CurrentPreproc}"
 
 if [ ! -d ${CurrentLog} ]; then mkdir -p ${CurrentLog}; chmod 770 ${CurrentLog}; fi
 
-# Loop over participants & sessions (if they exist)
+Loop over participants & sessions (if they exist)
 for SUB in ${SubjectID} ; do
 	if [ -z "${SessionID}" ]; then Session="NoSessions"; SessionFolder=""; SessionName=""
 	else Session="${SessionID}"
@@ -26,40 +26,27 @@ for SUB in ${SubjectID} ; do
 		if [ "${Session}" != "NoSessions" ]; then
 			if [ ! -d ${ProjectDirectory}/data/${SUB}/${SES} ]; then continue
 			else SessionFolder="${SES}/"; SessionName="${SES}_"
-			fi			
+			fi			dddd
 		fi	
 		
 		# Path to the anatomical image folder.
-		AnatPath="${ProjectDirectory}/data/${SUB}/${SessionFolder}mri/t1"		# Path for anatomical image	
-		# Name of anatomical image to be used.
-		AnatImage="${SUB}_${SessionName}t1" 									# Original anatomical image, no extraction performed
+		FieldPath="${ProjectDirectory}/data_renamed/${SUB}/mri/t1"		# Path for field images	
+		
+		# Name of phase image to be used.
+		PhaseImage="${SUB}_grephase" 	
+		
+		# Name of magnitudue image to be used
+		MagImage="${SUB}_gremag" 
+		
+		#divide by two
+		
 		# Output path for ANTs procedure
-		ANTsPath="${AnatPath}/tempANTs/${AnatImage}_ANTs_"
+		#ANTsPath="${AnatPath}/tempANTs/${AnatImage}_ANTs_"
+		
 		# Start log
-		StartLog="${ANTsPath}started.txt"
+		StartLog="${FMpath}started.txt"
 		# Error message if ANTs did not produce the expected output
-		CrashLog="${ANTsPath}failed.txt"
-		
-		# If anat files have not been properly renamed.
-		cd ${AnatPath}
-		if [ ! -f ${AnatImage}.nii.gz ]; then
-			AnatImage=`ls co*`														
-		fi
-		
-		if [ ! -f ${AnatPath}/${AnatImage}.nii.gz ]; then   		# Verifies if the anatomical image exists. If it doesn't, the for loop stops here and continues with the next item. 
-			echo "No mprage: ${SUB} cannot be processed"
-			continue
-		elif [ -f ${AnatPath}/${AnatImage}_brain.nii.gz ]; then 	# Verify if brain extracted image has been selected, if so, stop here.
-			continue
-		elif [ -f ${ANTsPath}BrainExtractionBrain.nii.gz ]; then 	# Verify if ANTs output was already created
-			continue
-		elif [ ! -f ${ANTsPath}BrainExtractionBrain.nii.gz ]; then
-			if [ -f ${CrashLog} ]; then 							# Verify if crash log exists, if so, delete intermediary ANTs files and re-run ANTs.
-				rm -rf ${AnatPath}/tempANTs 				
-			elif [ -f ${StartLog} ]; then 							# Verify if ANTs job started. Could be problematic if job did not finish.
-				continue
-			fi
-		fi
+		CrashLog="${FMpath}failed.txt"
 		
 		# ANTs-specific file paths
 		TemplatePath="${ANTsPath}/${SelectedTemplate}" 					# Directory for ANTs template to be used.
@@ -77,21 +64,22 @@ for SUB in ${SubjectID} ; do
     	
 		echo "sleep $(( RANDOM % 120 ))"						>> job # Sleep for a random period between 1-60 seconds, used to avoid interfierence when running antsBrainExtraction.sh
 		     	
-    	echo "module load ants/2.2.0" 							>> job
+    		echo "module load ants/2.2.0" 							>> job
 		
 		# Create temporary folder.
-		echo "mkdir ${AnatPath}/tempANTs"						>> job
+		echo "mkdir ${FMPath}/tempANTs"						>> job
 		echo "echo 'ANTs will start now' >> ${StartLog}"		>> job
 		
 		# Perform Brain Extraction
 		
-		echo -n "antsBrainExtraction.sh -d ${ImageDimension} -a ${AnatPath}/${AnatImage}.nii.gz -e ${TemplateImage} " 	>> job
-		echo  "-m ${ProbabilityImage} -f ${RegistrationMask} -k ${KeepTemporaryFiles} -o ${ANTsPath}" 					>> job
+		echo -n "antsBrainExtraction.sh -d 3 -a ${FMPath}/${AnatImage}.nii.gz -e ${TemplateImage} " 	>> job
+		echo  "-m ${ProbabilityImage} -f ${RegistrationMask} -k ${KeepTemporaryFiles} -o ${FMPath}" 					>> job
 
 		# If the final ANTs output isn't created, write a text file to be used as a verification of the output outcome.
 		echo "if [ ! -f ${ANTsPath}BrainExtractionBrain.nii.gz ]; then echo 'BrainExtractionBrain file was not produced.' >> ${CrashLog}; fi" >> job
 
 ${FSLDIR}/bin/imcp ${PhaseInputName} ${WD}/Phase
+
 ${FSLDIR}/bin/fsl_prepare_fieldmap SIEMENS ${WD}/Phase ${WD}/Magnitude_brain   2.46
 
 		echo "cd ${AnatPath}"									>> job
