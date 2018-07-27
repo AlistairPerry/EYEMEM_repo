5d0992e70f2b71323696d96472b88db8d1708917
Perry <alistairperry123@gmail.com>
2018-07-27 13:30:18 +0200

    updating dir structure

--- B_preproc_prepareFM.sh
+++ B_preproc_prepareFM.sh
@@ -1,47 +1,28 @@
 #!/bin/bash
 
-#### From HCP Pipeline ###
-
-${FSLDIR}/bin/fslmaths ${MagnitudeInputName} -Tmean ${WD}/Magnitude
-
-### They say BET, I'm going to use ANTs bra
-
-#${FSLDIR}/bin/bet ${WD}/Magnitude ${WD}/Magnitude_brain -f 0.35 -m #Brain extract the magnitude image
-
-
-source preproc_config.sh
+source preproc2_config.sh
 
 # PBS Log Info
-CurrentPreproc="BET/ANTs"
+CurrentPreproc="greprep"
 CurrentLog="${LogPath}/${CurrentPreproc}"
 
 if [ ! -d ${CurrentLog} ]; then mkdir -p ${CurrentLog}; chmod 770 ${CurrentLog}; fi
 
 Loop over participants & sessions (if they exist)
+
 for SUB in ${SubjectID} ; do
-	if [ -z "${SessionID}" ]; then Session="NoSessions"; SessionFolder=""; SessionName=""
-	else Session="${SessionID}"
-	fi
-	for SES in ${Session}; do
-		if [ "${Session}" != "NoSessions" ]; then
-			if [ ! -d ${ProjectDirectory}/data/${SUB}/${SES} ]; then continue
-			else SessionFolder="${SES}/"; SessionName="${SES}_"
-			fi			dddd
-		fi	
 		
-		# Path to the anatomical image folder.
-		FieldPath="${ProjectDirectory}/data_renamed/${SUB}/mri/t1"		# Path for field images	
+		# Path to the gre image folder.
+		FieldOrigPath="${ProjectDirectory}/data_renamed/${SUB}/mri/gre"		# Path for field images	
 		
 		# Name of phase image to be used.
 		PhaseImage="${SUB}_grephase" 	
 		
 		# Name of magnitudue image to be used
 		MagImage="${SUB}_gremag" 
-		
-		#divide by two
-		
-		# Output path for ANTs procedure
-		#ANTsPath="${AnatPath}/tempANTs/${AnatImage}_ANTs_"
+				
+		# Output path for field map prep procedure
+		ANTsPath="${AnatPath}/tempANTs/${AnatImage}_ANTs_"
 		
 		# Start log
 		StartLog="${FMpath}started.txt"
@@ -56,7 +37,7 @@ for SUB in ${SubjectID} ; do
 		
 		# Gridwise
 		echo "#PBS -N ${CurrentPreproc}_${SessionName}${SUB}" 	>> job # Job name 
-		echo "#PBS -l walltime=03:00:00" 						>> job # Time until job is killed 
+		echo "#PBS -l walltime=12:00:00" 						>> job # Time until job is killed 
 		echo "#PBS -l mem=10gb" 								>> job # Books 10gb RAM for the job 
 		echo "#PBS -m n" 										>> job # Email notification on abort/end, use 'n' for no notification 
 		echo "#PBS -o ${CurrentLog}" 							>> job # Write (output) log to group log folder 
@@ -70,19 +51,30 @@ for SUB in ${SubjectID} ; do
 		echo "mkdir ${FMPath}/tempANTs"						>> job
 		echo "echo 'ANTs will start now' >> ${StartLog}"		>> job
 		
+		#### From HCP Pipeline ###
+		#divide by two
+
+		echo "fslmaths ${MagnitudeInputName} -Tmean ${FMPath}/Magnitude" 		>> job
+
+		### They say BET, I'm going to use ANTs bra
+
+		#${FSLDIR}/bin/bet ${WD}/Magnitude ${WD}/Magnitude_brain -f 0.35 -m #Brain extract the magnitude image
+		
 		# Perform Brain Extraction
 		
 		echo -n "antsBrainExtraction.sh -d 3 -a ${FMPath}/${AnatImage}.nii.gz -e ${TemplateImage} " 	>> job
 		echo  "-m ${ProbabilityImage} -f ${RegistrationMask} -k ${KeepTemporaryFiles} -o ${FMPath}" 					>> job
 
 		# If the final ANTs output isn't created, write a text file to be used as a verification of the output outcome.
-		echo "if [ ! -f ${ANTsPath}BrainExtractionBrain.nii.gz ]; then echo 'BrainExtractionBrain file was not produced.' >> ${CrashLog}; fi" >> job
+		echo "if [ ! -f ${FMPath}/BrainExtractionBrain.nii.gz ]; then echo 'BrainExtractionBrain file was not produced.' >> ${CrashLog}; fi" >> job
 
-${FSLDIR}/bin/imcp ${PhaseInputName} ${WD}/Phase
+		echo "mv ${FMPath}/BrainExtractionBrain.nii.gz ${FMPath}/Magnitude_brain.nii.gz"		>> job
 
-${FSLDIR}/bin/fsl_prepare_fieldmap SIEMENS ${WD}/Phase ${WD}/Magnitude_brain   2.46
+		#${FSLDIR}/bin/imcp ${PhaseInputName} ${WD}/Phase
 
-		echo "cd ${AnatPath}"									>> job
+		echo "fsl_prepare_fieldmap SIEMENS ${PhaseInputName} ${FMPath}/Magnitude_brain 2.46"		>> job
+
+		echo "cd ${FMPath}"									>> job
 		echo "chmod -R 770 ."  									>> job
 
 		qsub job
